5edf491d7c2348346b7b0ff6acc578294f90baae
Calvin Wilkinson <kinsondigital@gmail.com>
2023-04-06 14:28:14 +0100

    chore: update head tag description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,8 +23,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="2D Game Development Framework">
       <HomepageHeader />
       <main>
         <HomepageFeatures />

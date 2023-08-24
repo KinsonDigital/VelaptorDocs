9a9b2f7fa3007b55a057e26aae5e371d0a99561e
Calvin Wilkinson <kinsondigital@gmail.com>
2023-08-24 12:17:07 +0100

    📋Elevate push permissions to api release (#100)
    * Start work for issue #99
    
    * ci: use token for checkout step to elevate git permissions

--- .github/cicd/scripts/velaptor-version-exists.ts
+++ .github/cicd/scripts/velaptor-version-exists.ts
@@ -1,13 +1,33 @@
 import { TagClient } from "clients/TagClient.ts";
 
-if (Deno.args.length < 1) {
-	const errorMsg = "No arguments exist.  Please provide a velaptor version to check.";
+if (Deno.args.length < 2) {
+	const errorMsg = `The required number of arguments is 2 but received ${Deno.args.length}.`;
 	console.log(`::error::${errorMsg}`);
 	Deno.exit(1);
 }
 
+const containsPAT = (value: string): boolean => {
+    const fineGrainedTokenPrefix = "github_pat_";
+    const classicTokenPrefix = "ghp_";
+
+    return value.startsWith(fineGrainedTokenPrefix) || value.startsWith(classicTokenPrefix);
+}
+
 const versionRegex = /^v[0-9]+\.[0-9]+\.[0-9]+(|-preview\.[0-9]+)$/gm;
 const version = Deno.args[0].trim().toLowerCase();
+const token = Deno.args[1].trim();
+
+if (containsPAT(version)) {
+	const errorMsg = "The version cannot contain a GitHub PAT.";
+	console.log(`::error::${errorMsg}`);
+	Deno.exit(1);
+}
+
+if (!containsPAT(token)) {
+	const errorMsg = "The 2nd argument must be a GitHub PAT.";
+	console.log(`::error::${errorMsg}`);
+	Deno.exit(1);
+}
 
 if (!versionRegex.test(version)) {
 	const errorMsg = `The version '${version}' is not a valid version.`;
@@ -15,7 +35,7 @@ if (!versionRegex.test(version)) {
 	Deno.exit(1);
 }
 
-const tagClient: TagClient = new TagClient();
+const tagClient: TagClient = new TagClient(token);
 
 const versionDoesNotExist = !(await tagClient.tagExists("Velaptor", version));
 

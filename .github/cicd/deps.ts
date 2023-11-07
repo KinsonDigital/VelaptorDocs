dc796f93e484ce85f7b033ad94712a072a381f1d
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-07 10:44:07 +0000

    deps: update NuGetClient dependency

--- .github/cicd/deps.ts
+++ .github/cicd/deps.ts
@@ -2,7 +2,7 @@ import { emptyDirSync, copySync, ensureDirSync } from "https://deno.land/std@0.2
 import { resolve, dirname, extname } from "https://deno.land/std@0.205.0/path/mod.ts";
 import { Input, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
 import { TagClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.3/GitHubClients/TagClient.ts";
-import { NuGetClient } from "https://raw.githubusercontent.com/kinsondigital/Infrastructure/v12.1.0/cicd/clients/NuGetClient.ts";
+import { NuGetClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.3/PackageClients/NuGetClient.ts";
 
 export { emptyDirSync, copySync, ensureDirSync };
 export { resolve, dirname, extname };

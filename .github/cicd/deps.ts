cf66cb133b0c8cb46e52602e4250995db23d7910
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-07 10:42:21 +0000

    deps: remove unused dependency

--- .github/cicd/deps.ts
+++ .github/cicd/deps.ts
@@ -3,9 +3,8 @@ import { resolve, dirname, extname } from "https://deno.land/std@0.205.0/path/mo
 import { Input, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
 import { TagClient } from "https://raw.githubusercontent.com/kinsondigital/Infrastructure/v12.1.0/cicd/clients/TagClient.ts";
 import { NuGetClient } from "https://raw.githubusercontent.com/kinsondigital/Infrastructure/v12.1.0/cicd/clients/NuGetClient.ts";
-import { WorkflowClient } from "https://raw.githubusercontent.com/kinsondigital/Infrastructure/v12.1.0/cicd/clients/WorkflowClient.ts";
 
 export { emptyDirSync, copySync, ensureDirSync };
 export { resolve, dirname, extname };
 export { Input, Select };
-export { TagClient, NuGetClient, WorkflowClient };
+export { TagClient, NuGetClient };

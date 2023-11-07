9ac7a72810321f0e48cf8049a2498fe3498ddad2
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-07 10:30:03 +0000

    deps: remove deprecated and unused function

--- .github/cicd/deps.ts
+++ .github/cicd/deps.ts
@@ -1,6 +1,5 @@
-import { emptyDirSync, copySync, ensureDirSync } from "https://deno.land/std@0.203.0/fs/mod.ts"
-import { resolve, dirname, extname } from "https://deno.land/std@0.203.0/path/mod.ts"
-import { writeAllSync } from "https://deno.land/std@0.203.0/streams/write_all.ts"
+import { emptyDirSync, copySync, ensureDirSync } from "https://deno.land/std@0.205.0/fs/mod.ts";
+import { resolve, dirname, extname } from "https://deno.land/std@0.205.0/path/mod.ts";
 import { Input, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
 import { TagClient } from "https://raw.githubusercontent.com/kinsondigital/Infrastructure/v12.1.0/cicd/clients/TagClient.ts";
 import { NuGetClient } from "https://raw.githubusercontent.com/kinsondigital/Infrastructure/v12.1.0/cicd/clients/NuGetClient.ts";
@@ -8,6 +7,5 @@ import { WorkflowClient } from "https://raw.githubusercontent.com/kinsondigital/
 
 export { emptyDirSync, copySync, ensureDirSync };
 export { resolve, dirname, extname };
-export { writeAllSync };
 export { Input, Select };
 export { TagClient, NuGetClient, WorkflowClient };

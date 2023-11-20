953bf37674f34bd1d71923b68a5cfe96751edeed
Calvin Wilkinson <85414302+CalvinWilkinson@users.noreply.github.com>
2023-11-20 22:23:49 +0000

    📋Update kd_clients dependency (#152)
    * Start work for issue #136
    
    * deps: update kd_clients from v1.0.0-preview.3 to v1.0.0-preview.5

--- .github/cicd/deps.ts
+++ .github/cicd/deps.ts
@@ -1,8 +1,8 @@
 import { emptyDirSync, copySync, ensureDirSync } from "https://deno.land/std@0.205.0/fs/mod.ts";
 import { resolve, dirname, extname } from "https://deno.land/std@0.205.0/path/mod.ts";
 import { Input, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
-import { TagClient, RepoClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.3/GitHubClients/mod.ts";
-import { NuGetClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.3/PackageClients/NuGetClient.ts";
+import { TagClient, RepoClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
+import { NuGetClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/PackageClients/NuGetClient.ts";
 
 export { emptyDirSync, copySync, ensureDirSync };
 export { resolve, dirname, extname };

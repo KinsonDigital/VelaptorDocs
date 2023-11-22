32792304b09f8595b99ab53b7063802b3071c4e9
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-22 13:57:43 +0000

    deps: update deno standard modules from v0.205.0 to v0.207.0

--- .github/cicd/deps.ts
+++ .github/cicd/deps.ts
@@ -1,5 +1,5 @@
-import { emptyDirSync, copySync, ensureDirSync } from "https://deno.land/std@0.205.0/fs/mod.ts";
-import { resolve, dirname, extname } from "https://deno.land/std@0.205.0/path/mod.ts";
+import { emptyDirSync, copySync, ensureDirSync } from "https://deno.land/std@0.207.0/fs/mod.ts";
+import { resolve, dirname, extname } from "https://deno.land/std@0.207.0/path/mod.ts";
 import { Input, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
 import { TagClient, RepoClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
 import { NuGetClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/PackageClients/NuGetClient.ts";

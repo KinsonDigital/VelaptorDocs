d2bc6df041693417f177f0b46a6ea36ad21e8400
Calvin Wilkinson <kinsondigital@gmail.com>
2024-04-20 11:38:06 +0100

    chore: improve deps.ts file

--- .github/cicd/deps.ts
+++ .github/cicd/deps.ts
@@ -1,3 +1,4 @@
+// THIRD PARTY MODULE IMPORTS
 import { copySync, emptyDirSync, ensureDirSync, existsSync, walkSync } from "https://deno.land/std@0.207.0/fs/mod.ts";
 import { basename, dirname, extname, parse, resolve } from "https://deno.land/std@0.207.0/path/mod.ts";
 import { Input, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.4/prompt/mod.ts";
@@ -5,6 +6,9 @@ import { RepoClient, TagClient } from "https://deno.land/x/kd_clients@v1.0.0-pre
 import { NuGetClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/PackageClients/NuGetClient.ts";
 import { crayon } from "https://deno.land/x/crayon@3.3.3/mod.ts";
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// THIRD PARTY MODULE EXPORTS
 export { copySync, emptyDirSync, ensureDirSync, existsSync, walkSync };
 export { basename, dirname, extname, parse, resolve };
 export { Input, Select };

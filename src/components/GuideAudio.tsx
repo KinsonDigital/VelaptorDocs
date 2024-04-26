c93258ae205240db5757adfb288e64378e563056
Calvin Wilkinson <kinsondigital@gmail.com>
2024-04-26 12:57:04 +0100

    chore: create custom component to download and play guide audio

---
+++ src/components/GuideAudio.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Audio from "@site/src/components/Audio";
+
+/**
+ * The properties for the {@link GuidImg} component.
+ */
+interface Props {
+    /**
+     * The version of the Velaptor API to use.
+     * @remarks If not provided, then the main branch will be used.
+     */
+    apiVersion: string;
+
+    /**
+     * The name of the guide project.
+     */
+    guideName: string,
+
+    /**
+     * The name of the image file.
+     */
+    audioFileName: string,
+
+    /**
+     * The name to give the audio player.
+     */
+    name?: string,
+}
+
+/**
+ * Creates a simple audio playback component to play a sound located at the given {@link url}.
+ * @param params The component properties.
+ * @returns The component.
+ */
+const GuideAudio: React.FC<Props> = ({ apiVersion, guideName, audioFileName, name}: Props) => {
+    const versionRegex = /^\s*v([1-9]\d*|0)\.([1-9]\d*|0)\.([1-9]\d*|0)(-preview\.([1-9]\d*))?\s*$/gm;
+    const invalidGuideProjName = guideName === undefined || guideName === "";
+    const undefinedOrEmptyFileName = audioFileName === undefined || audioFileName === "";
+    
+    let errorMsg = undefined;
+    
+    if (!versionRegex.test(apiVersion)) {
+        errorMsg = "The api version is not a valid prod or prev version.";
+    } else if (invalidGuideProjName) {
+        errorMsg = "You must provide a valid C# guide project name.";
+    } else if (undefinedOrEmptyFileName) {
+        errorMsg = "You must provide a valid audio file name.";
+    }
+
+    const fileSections = audioFileName.toLowerCase().split(".");
+
+    const fileExtension = fileSections.length >= 2 ? fileSections[fileSections.length - 1] : "";
+    const invalidFileName = fileExtension != "" && fileExtension != "mp3" && fileExtension != "ogg";
+
+    if (invalidFileName) {
+        errorMsg = "The audio file must be an '.mp3' or '.ogg' file.";
+    }
+
+    const relativeImgPath = `SampleProjects/Guides/${guideName}/Content/Audio/${audioFileName}`;
+    const url = `https://raw.githubusercontent.com/KinsonDigital/VelaptorDocs/api-${apiVersion}/${relativeImgPath}`;
+
+	return (
+		<div>
+            {
+                errorMsg !== undefined
+                    ? <div className="error">{errorMsg}</div>
+                    : <Audio url={url} name={name} />
+            }
+		</div>
+	);
+}
+
+export default GuideAudio;

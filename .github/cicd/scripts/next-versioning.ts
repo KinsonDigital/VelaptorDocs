74d24cea3e3d8d10037700936f4080fc59b5f0ae
Calvin Wilkinson <kinsondigital@gmail.com>
2024-05-03 19:05:41 +0100

    chore: disable next versioning

--- .github/cicd/scripts/next-versioning.ts
+++ .github/cicd/scripts/next-versioning.ts
@@ -1,50 +1,52 @@
 import { existsSync } from "../deps.ts";
 
-// Check the arguments
-if (Deno.args.length < 2) {
-	let errorMsg = "There must be 2 arguments";
-	errorMsg += "Usage: deno run FlagService.ts <file-path> <flag-id> <[enable | disable]>";
+throw new Error("Next versioning has been disabled");
 
-	throw Error(errorMsg);
-}
+// // Check the arguments
+// if (Deno.args.length < 2) {
+// 	let errorMsg = "There must be 2 arguments";
+// 	errorMsg += "Usage: deno run FlagService.ts <file-path> <flag-id> <[enable | disable]>";
 
-const filePath = Deno.args[0].trim();
+// 	throw Error(errorMsg);
+// }
 
-// Validate the file path argument
-if (!existsSync(filePath, { isFile: true })) {
-	throw Error(`The file path '${filePath}' does not exist.`);
-}
+// const filePath = Deno.args[0].trim();
 
-const setting: "enable" | "disable" = <"enable" | "disable">Deno.args[1];
+// // Validate the file path argument
+// if (!existsSync(filePath, { isFile: true })) {
+// 	throw Error(`The file path '${filePath}' does not exist.`);
+// }
 
-// Validate the enable disable argument
-if (setting != "enable" && setting != "disable") {
-	throw Error(`The third argument must be either 'enable' or 'disable'.`);
-}
+// const setting: "enable" | "disable" = <"enable" | "disable">Deno.args[1];
 
-const baseDirPath = Deno.cwd();
-const docuConfigFileName = "docusaurus.config.js";
-const docuConfigFilePath = `${baseDirPath}/${docuConfigFileName}`;
+// // Validate the enable disable argument
+// if (setting != "enable" && setting != "disable") {
+// 	throw Error(`The third argument must be either 'enable' or 'disable'.`);
+// }
 
-const fileDoesNotExist = !existsSync(docuConfigFilePath, { isFile: true });
+// // const baseDirPath = Deno.cwd();
+// // const docuConfigFileName = "docusaurus.config.js";
+// // const docuConfigFilePath = `${baseDirPath}/${docuConfigFileName}`;
 
-if (fileDoesNotExist) {
-	const errorMsg = `The file '${docuConfigFileName}' does not exist in the directory '${baseDirPath}'.`;
-	console.error(errorMsg);
-	Deno.exit(1);
-}
+// const fileDoesNotExist = !existsSync(filePath, { isFile: true });
 
-const settingRegex = /includeCurrentVersion:\s(true|false).*/gm;
+// if (fileDoesNotExist) {
+// 	const errorMsg = `The file '${filePath}' does not exist.`;
+// 	console.error(errorMsg);
+// 	Deno.exit(1);
+// }
 
-let fileContent = Deno.readTextFileSync(docuConfigFilePath);
+// const settingRegex = /includeCurrentVersion:\s(true|false).*/gm;
 
-if (!settingRegex.test(fileContent)) {
-	const errorMsg = `The setting 'includeCurrentVersion' does not exist in the file '${docuConfigFileName}'.` +
-		"\nThe setting is enabled by default if it does not exist.";
-	console.error(errorMsg);
-	Deno.exit(1);
-}
+// let fileContent = Deno.readTextFileSync(filePath);
 
-const replacement = setting === "enable" ? "includeCurrentVersion: true" : "includeCurrentVersion: false";
-fileContent = fileContent.replace(settingRegex, replacement);
-Deno.writeTextFileSync(docuConfigFilePath, fileContent);
+// if (!settingRegex.test(fileContent)) {
+// 	const errorMsg = `The setting 'includeCurrentVersion' does not exist in the file '${filePath}'.` +
+// 		"\nThe setting is enabled by default if it does not exist.";
+// 	console.error(errorMsg);
+// 	Deno.exit(1);
+// }
+
+// const replacement = setting === "enable" ? "includeCurrentVersion: true" : "includeCurrentVersion: false";
+// fileContent = fileContent.replace(settingRegex, replacement);
+// Deno.writeTextFileSync(filePath, fileContent);

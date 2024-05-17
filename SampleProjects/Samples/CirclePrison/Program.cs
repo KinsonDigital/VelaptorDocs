297240e438a7cdbcc5929e2ab871a81aab615406
Calvin Wilkinson <kinsondigital@gmail.com>
2024-05-17 13:05:39 +0100

    chore: add sample project

---
+++ SampleProjects/Samples/CirclePrison/Program.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="Program.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using CirclePrison;
+
+var game = new Game();
+game.Show();

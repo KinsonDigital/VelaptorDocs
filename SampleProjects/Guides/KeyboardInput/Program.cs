8dc50bd833e0a77f2266f2401bcf12f7fb7dd3dd
Calvin Wilkinson <kinsondigital@gmail.com>
2024-05-20 10:18:24 +0100

    docs: adjust style cop company value

--- SampleProjects/Guides/KeyboardInput/Program.cs
+++ SampleProjects/Guides/KeyboardInput/Program.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+﻿// <copyright file="Program.cs" company="KinsonDigital">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 

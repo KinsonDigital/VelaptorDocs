1121c166100e78c363aa7b8e16f77db8dad2e535
CalvinWilkinson <kinsondigital@gmail.com>
2024-09-05 18:25:22 +0100

    deps: update kd_clients to version v1.0.0-preview.13

--- .github/cicd/deps.ts
+++ .github/cicd/deps.ts
@@ -2,8 +2,8 @@
 import { copySync, emptyDirSync, ensureDirSync, existsSync, walkSync } from "https://deno.land/std@0.224.0/fs/mod.ts";
 import { basename, dirname, extname, parse, resolve } from "https://deno.land/std@0.224.0/path/mod.ts";
 import { Input, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.4/prompt/mod.ts";
-import { RepoClient, TagClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
-import { NuGetClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/PackageClients/NuGetClient.ts";
+import { RepoClient, TagClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.13/GitHubClients/mod.ts";
+import { NuGetClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.13/PackageClients/NuGetClient.ts";
 import { crayon } from "https://deno.land/x/crayon@3.3.3/mod.ts";
 
 // RAW GITHUB IMPORTS

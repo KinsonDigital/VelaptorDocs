ad913de50dd6715ecb8d962cc0fa275260e97af0
CalvinWilkinson <kinsondigital@gmail.com>
2024-09-05 18:17:01 +0100

    deps: update deno versions to v0.224.0

--- .github/cicd/deps.ts
+++ .github/cicd/deps.ts
@@ -1,6 +1,6 @@
 // THIRD PARTY MODULE IMPORTS
-import { copySync, emptyDirSync, ensureDirSync, existsSync, walkSync } from "https://deno.land/std@0.207.0/fs/mod.ts";
-import { basename, dirname, extname, parse, resolve } from "https://deno.land/std@0.207.0/path/mod.ts";
+import { copySync, emptyDirSync, ensureDirSync, existsSync, walkSync } from "https://deno.land/std@0.224.0/fs/mod.ts";
+import { basename, dirname, extname, parse, resolve } from "https://deno.land/std@0.224.0/path/mod.ts";
 import { Input, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.4/prompt/mod.ts";
 import { RepoClient, TagClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
 import { NuGetClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/PackageClients/NuGetClient.ts";

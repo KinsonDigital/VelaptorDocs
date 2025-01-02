002b257a2918327ff20fe4711e6e926702f75eaf
CalvinWilkinson <kinsondigital@gmail.com>
2025-01-02 18:44:42 +0000

    chore: update empty project code to match output of the VelEmpty project template

--- SampleProjects/Guides/EmptyProject/Program.cs
+++ SampleProjects/Guides/EmptyProject/Program.cs
@@ -1,14 +1,5 @@
-﻿// <copyright file="Program.cs" company="KinsonDigital">
-// Copyright (c) KinsonDigital. All rights reserved.
-// </copyright>
+﻿using EmptyProject;
 
-var game = new EmptyProject.Game();
-
-/* Show the game window.  This starts the game loop.
- * Any code below this point will not run until the Show() method has finished.
- * This is because the Show() method will not completed until the windows is closed.
- *
- * 💡You can use the ShowAsync() method if you want to show the window in an async
- * fashion so you can perform other processes while the window is loading.
-*/
+// Create a new game instance and run it to start the game
+var game = new Game();
 game.Show();

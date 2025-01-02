00ba760aaeea202c133e50dba7129dc443bbd48c
Calvin Wilkinson <kinsondigital@gmail.com>
2025-01-02 18:49:02 +0000

    ⚙️Update empty project to match proj template (#344)
    * Start work for issue #343
    
    * chore: update project run configs for rider
    
    * chore: update empty project code to match output of the VelEmpty project template

--- SampleProjects/Guides/EmptyProject/Game.cs
+++ SampleProjects/Guides/EmptyProject/Game.cs
@@ -7,76 +7,54 @@
 // ReSharper disable ArrangeConstructorOrDestructorBody
 namespace EmptyProject;
 
-using System.Diagnostics.CodeAnalysis;
 using Velaptor;
 using Velaptor.UI;
 
 /// <summary>
 /// The main game class.
 /// </summary>
-[SuppressMessage("csharpsquid", "S1135", Justification = "Intentional for doc site.")]
-[SuppressMessage("csharpsquid", "S1135", Justification = "Intentional for doc site.")]
-[SuppressMessage(
-    "StyleCop.CSharp.LayoutRules",
-    "SA1515:Single-line comment should be preceded by blank line",
-    Justification = "Intentional for doc site.")]
 public class Game : Window
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="Game"/> class.
-    /// </summary>
-    public Game()
-    {
-        Title = "Empty Project";
-        // TODO: Add other non content creation code here
-    }
-
-    /// <summary>
-    /// Loads game content.
+    /// Load content here.
     /// </summary>
     protected override void OnLoad()
     {
-        // TODO: Load game content here
+        base.OnLoad();
     }
 
     /// <summary>
-    /// Updates the application. Executes one time for every iteration of the game loop
-    /// and always BEFORE the <see cref="OnDraw"/> method.
+    /// Unload content here.
     /// </summary>
-    /// <param name="frameTime">The amount of time that has passed for the current frame.</param>
-    protected override void OnUpdate(FrameTime frameTime)
+    protected override void OnUnload()
     {
-        // TODO: Update game state here
-        base.OnUpdate(frameTime);
+        base.OnUnload();
     }
 
     /// <summary>
-    /// Invoked when the window is resized.  This includes minimizing, maximizing, and restoring the window.
+    /// Update size dependent related game objects and more here.
     /// </summary>
-    /// <param name="size">The new size the window.</param>
+    /// <param name="size">The new window size.</param>
     protected override void OnResize(SizeU size)
     {
-        // TODO: React to the window resizing here
         base.OnResize(size);
     }
 
     /// <summary>
-    /// Draws to the screen. Executes one time for every iteration of the game loop
-    /// and always AFTER the <see cref="OnUpdate"/> method has finished.
+    /// Add game logic here.
     /// </summary>
-    /// <param name="frameTime">The amount of time that has passed for the current frame.</param>
-    protected override void OnDraw(FrameTime frameTime)
+    /// <param name="frameTime">The amount of time that passed for the current game loop frame.</param>
+    protected override void OnUpdate(FrameTime frameTime)
     {
-        // TODO: Render game content here
-        base.OnDraw(frameTime);
+        base.OnUpdate(frameTime);
     }
 
     /// <summary>
-    /// Unload game content. Invoked when the window is closed.
+    /// Render graphics here.
     /// </summary>
-    protected override void OnUnload()
+    /// <param name="frameTime">The amount of time that passed for the current game loop frame.</param>
+    protected override void OnDraw(FrameTime frameTime)
     {
-        // TODO: Unload game content here
-        base.OnUnload();
+        base.OnDraw(frameTime);
     }
 }
